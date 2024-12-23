82b1546b549c46525ab5369a5c7f00713c70851c
JSEHAN04 <jshwow8304@gmail.com>
2024-12-23 14:51:02 +0900

    MovieInfo 페이지 컨트롤러
    포스터를 클릭하여 영화 상세 페이지로 이동

--- src/main/java/com/miraecinema/view/movieInfo/MovieInfoPageController.java
+++ src/main/java/com/miraecinema/view/movieInfo/MovieInfoPageController.java
@@ -1,4 +1,4 @@
-package com.miraecinema.view.review;
+package com.miraecinema.view.movieInfo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.miraecinema.biz.movie.MovieInfo;
 
 @Controller
-public class ReviewPageController {
-    @RequestMapping(value="/reviewPage.do", method = RequestMethod.GET)
+public class MovieInfoPageController {
+    @RequestMapping(value="/movieInfo.do", method = RequestMethod.GET)
     public String reviewPage(@ModelAttribute("movie") MovieInfo vo) {
         System.out.println("영화 상세 정보 페이지 파라메터 세팅");
         System.out.println("상영관 : " + vo.getRnum());
         System.out.println("영화 코드 : " + vo.getMovieCd());
-        return "reviewPage.jsp";
+        return "movieInfo.jsp";
     }
 }
