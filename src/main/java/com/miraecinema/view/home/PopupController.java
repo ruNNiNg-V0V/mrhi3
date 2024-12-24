c5eb030486929942e4de3ed8aa70f68cb1fe8fb2
Jihui0207 <abjhp0207@gmail.com>
2024-12-24 14:03:50 +0900

    footer재수정

--- src/main/java/com/miraecinema/view/home/PopupController.java
+++ src/main/java/com/miraecinema/view/home/PopupController.java
@@ -8,18 +8,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class PopupController {
 
-    @RequestMapping(value = "/popup/{type}", method = RequestMethod.GET)
-    public String getPopupContent(@PathVariable("type") String type) {
-        // 팝업 종류에 따라 반환할 JSP를 설정
-        switch (type) {
-            case "privacy":
-                return "popup/privacy"; // views/popup/privacy.jsp
-            case "terms":
-                return "popup/terms";   // views/popup/terms.jsp
-            case "reject":
-                return "popup/reject";   // views/popup/reject.jsp
-            default:
-                return "popup/notfound"; 
-        }
-    }
+	@RequestMapping(value = "/popup/{type}", method = RequestMethod.GET)
+	public String getPopupContent(@PathVariable("type") String type) {
+		// 팝업 종류에 따라 반환할 JSP를 설정
+		switch (type) {
+		case "notice":
+			return "popup/notice"; // views/popup/notice.jsp
+		case "privacy":
+			return "popup/privacy"; // views/popup/privacy.jsp
+		case "terms":
+			return "popup/terms"; // views/popup/terms.jsp
+		case "reject":
+			return "popup/reject"; // views/popup/reject.jsp
+		default:
+			return "popup/notfound";
+		}
+	}
 }
